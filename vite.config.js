38719b4a0bd706e40a1b217322394b9cf8c5e8de
sweriko <swerik.productions@gmail.com>
2025-08-06 19:32:04 +0100

    change config for build

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,14 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  // development server configuration
+  base: './', // Use relative paths for deployment
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: './index.html'
+      }
+    }
+  }
 });
