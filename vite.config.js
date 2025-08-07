bc5be79b5249869bcd0f8f87d5d7121c26892432
verekia <522007+verekia@users.noreply.github.com>
2025-08-07 11:34:23 +0200

    feat: WebGPU, TSL version

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,17 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
   // development server configuration
+  server: {
+    port: 5173,
+    open: false,
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: "index.html",
+        webgpu: "webgpu.html",
+      },
+    },
+  },
 });
